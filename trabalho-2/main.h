fa36ea192aa0a0d0847c66e4e88511a8bcd5f541
Bruna Caetano <bruna.caetano@acad.ufsm.br>
2023-09-04 22:08:26 -0300

    wip: horizontal com ponto foi

--- trabalho-2/main.h
+++ trabalho-2/main.h
@@ -12,13 +12,22 @@ diagonal, em qualquer sentido (direto ou reverso). */
 #include <stdbool.h>
 #include <ctype.h>
 
+typedef struct ponto {
+    int x, y, w, z;
+} PONTO;
+
+typedef struct palavra {
+    char *palavra;
+    PONTO point;
+} PALAVRA;
+
 typedef struct jogo {
     bool giveUp;
     int quantity;
     int heigth;
     int width;
     char **matriz;
-    char **finded;
+    PALAVRA *finded;
 } JOGO;
 
 // Mostra a matriz na tela
@@ -37,16 +46,16 @@ void printAllWords(JOGO partida);
 char *invert(char *s);
 
 // Verifica se a palavra esta na diagonal da matriz
-bool diagonal(JOGO partida, char *chute);
+PONTO diagonal(JOGO partida, char *chute);
 
 // Verifica se a palavra esta na vertical da matriz
-bool vertical(JOGO partida, char *chute);
+PONTO vertical(JOGO partida, char *chute);
 
 // Verifica se a palavra esta na horizontal da matriz
-bool horizontal(JOGO partida, char *chute);
+PONTO horizontal(JOGO partida, char *chute);
 
 // Busca a palavra na matriz
-bool search(JOGO partida, char *chute);
+PONTO search(JOGO partida, char *chute);
 
 // Processa um chute do usuario
 void process(JOGO partida, char *chute);
